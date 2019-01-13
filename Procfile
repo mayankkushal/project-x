b7516e2e406900d06ec566dc705e6e5c9268db80
Mayank Kushal <mayankkushal26@gmail.com.com>
2019-01-14 01:09:52 +0530

    for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn first_try.wsgi --log-file -
--- first_try/settings.py
+++ first_try/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,9 +26,10 @@ SECRET_KEY = '#+wwy)6@l7nv6kjjpr#9wp&nye!j(np1l(stuk=$v(2!2!y!&='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Application definition
 
 INSTALLED_APPS = [
