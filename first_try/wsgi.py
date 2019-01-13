3fbed9e08237c1fd05c4022aa6e8347339a40119
Mayank Kushal <mayankkushal26@gmail.com.com>
2019-01-14 01:30:00 +0530

    fix settings

--- first_try/wsgi.py
+++ first_try/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_try.settings')
 
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+application = get_wsgi_application()
