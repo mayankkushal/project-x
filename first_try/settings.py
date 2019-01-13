133a4398f235c1d6d436d8134eb297b16b9f3ef2
Mayank Kushal <mayankkushal26@gmail.com.com>
2019-01-14 01:19:08 +0530

    fix setting

--- first_try/settings.py
+++ first_try/settings.py
@@ -28,8 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 # Application definition
 
 INSTALLED_APPS = [
@@ -85,7 +83,8 @@ DATABASES = {
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
